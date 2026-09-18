396cf69b8e5af40bf90c53492c8bdde793eb3e57
Master System <mosescoopgroup1@gmail.com>
2026-09-18 13:35:11 +0800

    modified:   universal.js

--- universal.js
+++ universal.js
@@ -8,7 +8,7 @@
  * copy the new Web App URL and paste it as window.API below.
  */
 
-window.API = "https://script.google.com/macros/s/AKfycbzOtmtN4WPK3f4UWA4EFG_nsCCKZKdnPnoUn80JooUTeYdnmL__EXBke2OtY3EsVZJf/exec";
+window.API = "https://script.google.com/macros/s/AKfycbyfhDvE-4--LDuep6Bdox0KEgQUl1TJ6YzeG8dWZbpHaJMeFHxRXcNYK3vjWMDgtrq_/exec";
 
 // Must exactly match APP_SECRET in your Apps Script (Code.gs). Only change
 // this if you rotate the secret on the backend too.
